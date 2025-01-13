778496a145a2bfbdf6595e649f2419f7a9c0a6f9
Mohammad Halimi <mohammadhalimi.2001@gmail.com>
2025-01-14 01:33:08 +0330

    bug fixing 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const phoneValidator = require('./src/validators/phone');
-const postalCodeValidator = require('./src/validators/postalCode');
-const nationalIdValidator = require('./src/validators/nationalId');
+const phoneValidator = require('./src/validator/phone');
+const postalCodeValidator = require('./src/validator/postalCode');
+const nationalIdValidator = require('./src/validator/nationalId');
 
 const { regexPatterns } = require('./utils/regexPatterns');
 const { countryConfigs } = require('./utils/countryConfigs');
